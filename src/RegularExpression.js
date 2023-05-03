68d859704d1727bc08cb06e28602304e12d89cff
SulthanDA28 <13521159@std.stei.itb.ac.id>
2023-05-03 22:53:30 +0700

    add regex kalkulator and some stuff

--- src/RegularExpression.js
+++ src/RegularExpression.js
@@ -17,13 +17,20 @@ function processQuery(regexPatternList, query) {
     let category = matchRegEx(regexPatternList, query);
 
     if (category == 0) {
-        console.log("QUERY KALKULATOR");
-        console.log(evaluateExpression(query));
-    } else if (category == 1) {
         console.log("QUERY TANGGAL");
-        const [day, month, year] = query.split('/');
+        let hasil = query.match(regexPatternList[0][2]);
+        for(let i = 0;i<hasil[0].length;i++){
+            if(hasil[0][i]=='\-' || hasil[0][i]=='\\' || hasil[0][i]=='\.'){
+                hasil[0]=hasil[0].replace(hasil[0][i],'/');
+            }
+        }
+        const [day, month, year] = hasil[0].split('/');
         const date = new Date(`${month}/${day}/${year}`);
         console.log(getDayOfWeek(date));
+    } else if (category == 1) {
+        console.log("QUERY KALKULATOR");
+        let operation = query.match(regexPatternList[1][0]);
+        console.log(evaluateExpression(operation[0]));
     } else if (category == 2) {
         let i = 21;
         let pertanyaan = "";
@@ -47,24 +54,36 @@ function processQuery(regexPatternList, query) {
         }
     } else if (category == 3) {
         console.log("QUERY HAPUS PERTANYAAN");
+        if(checkPresence(query.substring(17))==false){
+            console.log("Pertanyaan tidak ditemukan!");
+        }
+        else{
+            deleteQuestion(query.substring(17));
+            console.log("Pertanyaan "+query.substring(17)+" telah dihapus!");
+        }
     } else {
         answerQuestionBM(query);
     }
 }
 
 
-// // TESTING
-// // Query untuk meminta hari dari tanggal
-const regexPattern11 = /(.*)?hari (apa )*(pada |di |untuk )*(tanggal )*(\d{1,2})[/\-.](\d{1,2})[/\-.](\d{2}|\d{4})(.*)?/gi;
-const regexPattern12 = /(pada )*(tanggal )*(\d{1,2})[/\-.](\d{1,2})[/\-.](\d{2}|\d{4})(.*)?hari (apa)*(.*)?/gi;
-const regexPattern13 = /(\d{1,2})[/\-.](\d{1,2})[/\-.](\d{2}|\d{4})/gi;
-const regexPattern21 = /Tambahkan pertanyaan .+ dengan jawaban .+/gi;
-const regexPattern31 = /Hapus pertanyaan .+/gi;
-const regexPatternList = [[], [regexPattern11, regexPattern12, regexPattern13], [regexPattern21], [regexPattern31]];
+// // Hasil Gabungan
+function realproccess(query){
+    const regexPattern01 = /(.*)?hari (apa )*(pada |di |untuk )*(tanggal )*(\d{1,2})[/\-.](\d{1,2})[/\-.](\d{2}|\d{4})(.*)?/gi;
+    const regexPattern02 = /(pada )*(tanggal )*(\d{1,2})[/\-.](\d{1,2})[/\-.](\d{2}|\d{4})(.*)?hari (apa)*(.*)?/gi;
+    const regexPattern03 = /(\d{1,2})[/\-.](\d{1,2})[/\-.](\d{2}|\d{4})/gi;
+    const regexPattern11 = /(([0-9])+[\+|\/|\-|\*]+)+([0-9])*/g;
+    const regexPattern12 = /([Bb]erapa ([Hh]asil )?)?(([Hh]asil) (dari))? (([0-9])+[\+|\/|\-|\*]+)+([0-9])*(\?)?/g;
+    const regexPattern21 = /Tambahkan pertanyaan .+ dengan jawaban .+/gi;
+    const regexPattern31 = /Hapus pertanyaan .+/gi;
+    const regexPatternList = [[regexPattern01,regexPattern02,regexPattern03], [regexPattern11, regexPattern12], [regexPattern21], [regexPattern31]];
+    processQuery(regexPatternList, query);
+}
 // console.log(matchRegEx(regexPatternList, "Tanggal 03-12-2022 itu hari apa sih ya? Tolong beritahu saya."));
 // console.log(matchRegEx(regexPatternList, "Hai, tau gak hari apa pada tanggal 06.02.2003?"));
 
 
 // TESTING 2
-let query = "Tambahkan pertanyaan apa tubes terseru stima? dengan jawaban yang paling seru adalah tubes 4";
-processQuery(regexPatternList, query);
+// let query = "hari apa pada tanggal 06-02-2003?";
+// let query = "Berapa hasil dari 2+3";
+// realproccess(query);
